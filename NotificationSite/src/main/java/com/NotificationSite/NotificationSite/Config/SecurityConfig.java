97c9aab7bd2018342993f5d063befe9774f4385f
Ko Bkei <101390721+Bkei12@users.noreply.github.com>
2023-08-14 22:15:59 +0900

    로그인, 로그아웃 기능 추가

--- NotificationSite/src/main/java/com/NotificationSite/NotificationSite/Config/SecurityConfig.java
+++ NotificationSite/src/main/java/com/NotificationSite/NotificationSite/Config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.NotificationSite.NotificationSite.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+// 스프링 시큐리티 설정 클래스
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -23,6 +26,13 @@ public class SecurityConfig {
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .formLogin((formLogin) -> formLogin
+                        .loginPage("/user/login")
+                        .defaultSuccessUrl("/"))
+                .logout((logout) -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true))
         ;
         return http.build();
     }
@@ -30,5 +40,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- NotificationSite/src/main/java/com/NotificationSite/NotificationSite/repository/UserRepository.java
+++ NotificationSite/src/main/java/com/NotificationSite/NotificationSite/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.NotificationSite.NotificationSite.repository;
 import com.NotificationSite.NotificationSite.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByusername(String username);
 }
