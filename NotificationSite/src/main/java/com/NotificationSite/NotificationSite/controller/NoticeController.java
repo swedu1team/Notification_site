0ab9c5546fbd71717186f078c04315e922d35c59
usingjun <97494494+usingjun@users.noreply.github.com>
2023-08-16 09:29:31 +0900

    notice_list 추가

--- NotificationSite/src/main/java/com/NotificationSite/NotificationSite/controller/NoticeController.java
+++ NotificationSite/src/main/java/com/NotificationSite/NotificationSite/controller/NoticeController.java
@@ -2,25 +2,29 @@ package com.NotificationSite.NotificationSite.controller;
 
 import com.NotificationSite.NotificationSite.entity.Notice;
 import com.NotificationSite.NotificationSite.service.NoticeService;
-import org.aspectj.weaver.ast.Not;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.management.loading.PrivateClassLoader;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class NoticeController {
     @Autowired
     private NoticeService noticeService;
 
+    @GetMapping
+    public String noticeList(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<Notice> paging = this.noticeService.getList(page);
+        model.addAttribute("paging", paging);
+        return "notice_list";
+    }
+
+
     //공지사항 작성 html과 연결
     @GetMapping("notice/noticewrite")
-    public String noticeWriteForm(){
+    public String noticeWriteForm() {
+
         return "noticewrite";
     }
 
@@ -28,7 +32,7 @@ public class NoticeController {
     @PostMapping("notice/noticewritepro")
     public String noticeWritePro(Notice notice){
         noticeService.write(notice);
-        return "noticelist";
+        return "notice_list";
     }
 
     //공지사항 상세
@@ -56,6 +60,6 @@ public class NoticeController {
 
         noticeService.write(temp);
 
-        return "noticelist";
+        return "notice_list";
     }
 }
--- NotificationSite/src/main/java/com/NotificationSite/NotificationSite/service/NoticeService.java
+++ NotificationSite/src/main/java/com/NotificationSite/NotificationSite/service/NoticeService.java
@@ -3,8 +3,14 @@ package com.NotificationSite.NotificationSite.service;
 import com.NotificationSite.NotificationSite.entity.Notice;
 import com.NotificationSite.NotificationSite.repository.NoticeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 @Service
 public class NoticeService {
     @Autowired
@@ -18,5 +24,12 @@ public class NoticeService {
     //공지사항 세부
     public Notice noticeView(Integer NOTICE_ID){
         return noticeRepository.findById(NOTICE_ID).get();
+    }
+
+    public Page<Notice> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("MEET_DAY"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.noticeRepository.findAll(pageable);
     }
 }
